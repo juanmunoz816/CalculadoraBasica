b4d3eca3a200384bb37f8bec00ecb11b2c91c118
juanmunoz816 <juanmunozrozo@gmail.com>
2022-12-09 07:17:01 -0500

    Calculadora básica

---
+++ js/form.js
@@ -0,0 +1,15 @@
+function calcula(operacion){
+    var operando1 = document.calc.operando1.value
+    var operando2 = document.calc.operando2.value
+
+    var result = eval(operando1 + operacion + operando2)
+    document.calc.resultado.value = result
+}
+
+function reset(){
+
+    var reset = 0
+
+    document.calc.operando1.value = reset
+    document.calc.operando2.value = reset
+}
